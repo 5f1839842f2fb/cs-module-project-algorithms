5a3d560bd4d8c8ca06f23a300420fad0c2238d03
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-08-12 20:08:01 -0400

    some

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,19 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    i1 = 0
+    i2 = 1
+    while i1 < len(arr) - 1:
+        while i2 < len(arr):
+            if arr[i1] == arr[i2]:
+                dupe1 = arr[i1]
+                arr.remove(dupe1)
+                arr.remove(dupe1)
+                i1 = 0
+                i2 = 1
+            else:
+                i2 += 1
+        return arr[0]
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
